89b7c0eda436557941f9d59500018270936dc797
yushchenko yuliia <yushchenko.yu@gmail.com>
2021-04-18 22:03:44 +0300

    install redux, redux-saga, react-redux, redux-wrapper and style-components

--- test_blog_developstoday/components/MainLayout.tsx
+++ test_blog_developstoday/components/MainLayout.tsx
@@ -1,19 +1,37 @@
 import Link from 'next/link'
 import { ROUTES } from '../constants/routes';
+import styled from 'styled-components';
 
+export default function MainLayout({ children }) {
 
-export default function MainLayout({children}) {
+    const Nav = styled.a`
+    position: fixed;
+    height: 60px;
+    left: 0;
+    top: 0;
+    right: 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: #fff;
+    text-decoration: none;
+    `
+    // const Main = styled.main`
+    // margin-top: 60px;
+    // padding: 1rem;
+    // `
+
     return (
         <>
-        <nav>
-        <Link href={ROUTES.home}><a>Home</a></Link>
-        <Link href={ROUTES.posts}><a>Posts</a></Link>
-        <Link href={ROUTES.newPost}><a>Add new post</a></Link>
+            <Nav>
+                <Link href={ROUTES.home}><a>Home</a></Link>
+                <Link href={ROUTES.posts}><a>Posts</a></Link>
+                <Link href={ROUTES.newPost}><a>Add new post</a></Link>
 
-        </nav>
-        <main>
-            {children}
-        </main>
+            </Nav>
+            <main>
+                {children}
+            </main>
         </>
     )
 }
